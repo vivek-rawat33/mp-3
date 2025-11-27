fd54ded7d9c9072f6d2b8b17c8a42db81e161b77
vivek <viveksr012@gmail.com>
2025-11-27 08:17:15 +0530

    change

--- guass_quadrature_integration.py
+++ guass_quadrature_integration.py
@@ -1,49 +1,29 @@
 # # Gauss quadrature method for integration
+#integral 1 
 import math as M
-# def func(x):
-#     return 5*x**3 - 4*x**2+ 7*x +10
+def func(x):
+    return 5*x**3 - 4*x**2+ 7*x +10
 
-# def gauss_quadrature(a, b):
-#     points = [-0.577, 0.577]
-#     weights = [1, 1]
+def gauss_quadrature(a, b):
+    points = [-0.577, 0.577]
+    weights = [1, 1]
 
-#     def to_interval(x):
-#         return ((b - a) / 2) * x + (a + b) / 2
+    def to_interval(x):
+        return ((b - a) / 2) * x + (a + b) / 2
 
-#     total = 0
-#     for i in range(2):
-#         x_val = to_interval(points[i])
-#         fx = func(x_val)  
-#         total += weights[i] * fx
-#     return ((b - a) / 2) * total
+    total = 0
+    for i in range(2):
+        x_val = to_interval(points[i])
+        fx = func(x_val)  
+        total += weights[i] * fx
+    return ((b - a) / 2) * total
 
-# result = gauss_quadrature(2, 5)
-# print("Result:", result)
-
-# a = int(input("Enter the lower limit :"))
-# b = int(input("Enter the upper limit :"))
-
-# def func(x):
-#     return 5*x**3 - 4*x**2 + 7*x + 10
-
-# def integral():
-#     x1 = 0.577
-#     x2 = -0.577
-#     w1 = w2 = 1
-
-#     p1 = ((b - a) / 2) * x1 + (a + b) / 2
-#     p2 = ((b - a) / 2) * x2 + (a + b) / 2
-
-#     ans = ((b - a) / 2) * (w1 * func(p1) + w2 * func(p2))
-#     return ans
-
-# result =integral()
-# print(result)
-
-
+result = gauss_quadrature(2, 5)
+print("Result:", result)
 a = int(input("Enter the lower limit :"))
 b = int(input("Enter the upper limit :"))
 
+#integral 2
 def func(x):
     return x**5 - 10*x**4 + 4*x**3 +  3*x**2+ 10
 
